63ff3bf51cba6e5a9c591863516756bdeccb48f4
Mathias Cabezas <mcabezas@utbm.fr>
2020-03-07 12:01:29 +0100

    nutriments begining

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,2 +1,10 @@
 class Burger < ApplicationRecord
 end
+class Burger < ApplicationRecord
+    def nutiments
+        # USE -> product.nutriments.to_hash
+        # Send nutiments to controler
+        # Make controler give it to the view (display only the "show burger" view)
+        # add it to json
+    end
+end
