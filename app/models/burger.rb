6c308ee89dae4726cd73f39e402c2c316219f587
Mathias Cabezas <mcabezas@utbm.fr>
2020-03-10 18:57:46 +0100

    managing null nutrments

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -10,6 +10,10 @@ class Burger < ApplicationRecord
         code = self.code
         product = Openfoodfacts::Product.get(code, locale: 'fr')
         @nut = product.nutriments
-        @nutrs = ["energy" => @nut.energy_100g, "fat" => @nut.fat_100g, "fat_unit" => @nut.fat_unit, "salt" => @nut.salt_100g, "salt_unit" => @nut.salt_unit, "proteins" => @nut.proteins_100g, "proteins_unit" => @nut.proteins_unit]
+        if @nut
+            @nutrs = ["energy" => @nut.energy_100g, "fat" => @nut.fat_100g, "fat_unit" => @nut.fat_unit, "salt" => @nut.salt_100g, "salt_unit" => @nut.salt_unit, "proteins" => @nut.proteins_100g, "proteins_unit" => @nut.proteins_unit]
+        else
+            @nutrs = ["energy" => "?", "fat" => "?", "fat_unit" => "?", "salt" => "?", "salt_unit" => @nut.salt_unit, "proteins" => "?", "proteins_unit" => "?"]
+        end
     end
 end
