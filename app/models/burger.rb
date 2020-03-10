7738b1835b9af6f6cad2c8f4ad7572b584b98115
Mathias Cabezas <mcabezas@utbm.fr>
2020-03-10 18:40:41 +0100

    adding 'nutriments' from open food facts

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,10 +1,15 @@
 class Burger < ApplicationRecord
-end
-class Burger < ApplicationRecord
-    def nutiments
-        # USE -> product.nutriments.to_hash
+    def nutriments
+        # USE -> product.nutriments
         # Send nutiments to controler
         # Make controler give it to the view (display only the "show burger" view)
         # add it to json
+        require 'openfoodfacts'
+
+        # Browse a product
+        code = self.code
+        product = Openfoodfacts::Product.get(code, locale: 'fr')
+        @nut = product.nutriments
+        @nutriments = ["energy" => @nut.energy_100g, "fat" => @nut.fat_100g, "fat_unit" => @nut.fat_unit, "salt" => @nut.salt_100g, "salt_unit" => @nut.salt_unit, "proteins" => @nut.proteins_100g, "proteins_unit" => @nut.proteins_unit]
     end
 end
--- app/views/burgers/_burger.json.jbuilder
+++ app/views/burgers/_burger.json.jbuilder
@@ -1,2 +1,3 @@
 json.extract! burger, :id, :name, :price, :image, :nutriscore, :code, :created_at, :updated_at
+json.nutriments burger.nutriments
 json.url burger_url(burger, format: :json)
