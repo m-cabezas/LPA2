aed68b634e148a4990ced97f6ad9db8279df2c4d
Mathias Cabezas <mcabezas@utbm.fr>
2020-03-11 14:57:02 +0100

    automatisation of the nutriments

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,19 +1,15 @@
 class Burger < ApplicationRecord
     def nutriments
-        # USE -> product.nutriments
-        # Send nutiments to controler
-        # Make controler give it to the view (display only the "show burger" view)
-        # add it to json
         require 'openfoodfacts'
 
         # Browse a product
         code = self.code
         product = Openfoodfacts::Product.get(code, locale: 'fr')
-        @nut = product.nutriments
-        if @nut
-            @nutrs = ["energy" => @nut.energy_100g, "fat" => @nut.fat_100g, "fat_unit" => @nut.fat_unit, "salt" => @nut.salt_100g, "salt_unit" => @nut.salt_unit, "proteins" => @nut.proteins_100g, "proteins_unit" => @nut.proteins_unit]
+        
+        if product == nil
+            product = [["Unknow"], [""]]
         else
-            @nutrs = ["energy" => "?", "fat" => "?", "fat_unit" => "?", "salt" => "?", "salt_unit" => @nut.salt_unit, "proteins" => "?", "proteins_unit" => "?"]
-        end
+            product = product.nutriments
+        end        
     end
 end
